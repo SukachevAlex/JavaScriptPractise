7286db46cf007ab716eac6a4aa9d43d8440d6f82
SukachevAlex <sukachevalexandr@gmail.com>
2018-09-24 00:12:12 +0300

    Add function to play piano keyboard
    Add eventListener to each keyboard to play specify note, after key was pressed add 'key_pressed' class for transition time

--- day-01/src/index.js
+++ day-01/src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+const keys = document.querySelectorAll('.key');
 
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('keydown', playSound);
+    keys.forEach(el => el.addEventListener('transitionend', removeClass));
 });
+
+function playSound(e) {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        const audio = new Audio(`sounds/${e.keyCode}.wav`);
+        audio.play();
+        const key = document.querySelector(`.key[data-key="${e.keyCode}`);
+        key.classList.add('key_pressed');
+    }
+}
+
+function removeClass(e) {
+    this.classList.remove('key_pressed');
+}
